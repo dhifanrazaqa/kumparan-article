7770a3816e75e4cbf38afebfb7d1883e10e0d3fc
dhifanrazaqa <dhifanofficial@gmail.com>
2025-06-21 14:47:27 +0700

    fix: change json name for password

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type User struct {
-	ID             string `json:"id"`
-	Username       string `json:"username"`
-	HashedPassword string `json:"-"`
+	ID             string    `json:"id"`
+	Username       string    `json:"username"`
+	HashedPassword string    `json:"-"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"`
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
@@ -24,5 +24,5 @@ type CreateUserRequest struct {
 
 type UpdateUserRequest struct {
 	Username string `json:"username" validate:"omitempty,min=3,max=50"`
-	Password string `json:"hashed_password" validate:"omitempty,min=8,max=100"`
+	Password string `json:"password" validate:"omitempty,min=8,max=100"`
 }
